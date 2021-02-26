5955056e885773a2bce8061cb632f426ad0fa434
Matvei Moroz <minilooth@gmail.com>
2021-02-26 19:36:58 +0300

    minor improvements

---
+++ src/main/java/by/minilooth/telegrambot/bot/api/enums/UpdateType.java
@@ -0,0 +1,18 @@
+package by.minilooth.telegrambot.bot.api.enums;
+
+public enum UpdateType {
+    
+    CALLBACK_QUERY,
+    TEXT,
+    AUDIO,
+    DOCUMENT,
+    PHOTO,
+    VIDEO,
+    VIDEO_NOTE,
+    VOICE,
+    CONTACT,
+    ANIMATION,
+    STICKER,
+    LOCATION;
+
+}
---
+++ src/main/java/by/minilooth/telegrambot/bot/message/client/ClientMessageService.java
@@ -0,0 +1,50 @@
+package by.minilooth.telegrambot.bot.message.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import by.minilooth.telegrambot.bot.api.enums.UpdateType;
+import by.minilooth.telegrambot.bot.context.client.ClientBotContext;
+import by.minilooth.telegrambot.bot.keyboard.client.ClientReplyKeyboardMarkupSource;
+import by.minilooth.telegrambot.bot.message.MessageService;
+import by.minilooth.telegrambot.bot.api.MessageSender;
+import by.minilooth.telegrambot.exception.ClientNotFoundException;
+import by.minilooth.telegrambot.model.Client;
+import by.minilooth.telegrambot.util.BotUtils;
+
+@Service
+public class ClientMessageService extends MessageService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ClientMessageService.class);
+    
+    @Autowired private BotUtils botUtils;
+    @Autowired private MessageSender messageSender;
+    @Autowired private ClientMessageSource clientMessageSource;
+    @Autowired private ClientReplyKeyboardMarkupSource clientReplyKeyboardMarkupSource;
+
+    public void sendStartMessage(ClientBotContext clientBotContext) throws ClientNotFoundException {
+        Client client = clientBotContext.getClient();
+
+        if (client == null) throw new ClientNotFoundException();
+
+        if (botUtils.getUpdateType(clientBotContext.getUpdate()).equals(UpdateType.CALLBACK_QUERY) &&
+            !clientBotContext.getUpdate().getMessage().getReplyMarkup().equals(new InlineKeyboardMarkup())) {
+
+        }
+        else {
+            try {
+                Message message = messageSender.sendMessage(client.getTelegramId(), clientMessageSource.getMessage("message"), null); 
+
+                updateLastBotMessage(client.getUser(), message);
+            }
+            catch (TelegramApiException ex) {
+                LOGGER.error("Unable to send start message to user: {0}, reason: {1}", client.getTelegramId(), ex.getLocalizedMessage());
+            }
+        }
+    }
+}
